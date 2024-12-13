8d5f747653ebee4ad0a39065e33860949883a828
HyunSeoJee <158494958+HyunSeoJee@users.noreply.github.com>
2024-12-13 03:01:24 -0500

    Add files via upload

---
+++ back.js
@@ -0,0 +1,10 @@
+function submit(){
+    let input = document.querySelector("#input");
+    let result = document.querySelector("#result");
+
+    result.innerHTML = input.value;
+}
+
+function customize(){
+    
+}
